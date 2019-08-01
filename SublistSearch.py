81932a9f4d10604007ff0c692b2b5b3bb22324a7
Amit Ranjan <40341658+amitalable@users.noreply.github.com>
2019-08-01 16:23:04 +0530

    Update SublistSearch.py

--- SublistSearch.py
+++ SublistSearch.py
@@ -7,25 +7,25 @@ class SublistSearch:
   def findList(self,first,second):
     ptr1 = first
     ptr2 = second
-    if first is None and Second is None:
-      return true
-    if first is None or Second is None:
-      return false
-    while second.dataval is not None:
+    if first is None and second is None:
+      return True
+    if first is None or second is None:
+      return False
+    while second is not None:
       ptr2.dataval = second.dataval
       while ptr1.dataval is not None:
         if ptr2.dataval is None:
-          return false
+          return False
         elif ptr2.dataval == ptr1.dataval:
-          ptr1 = ptr1.next
-          ptr2 = ptr2.next
+          ptr1 = ptr1.nextval
+          ptr2 = ptr2.nextval
         else:
           break;
-      if ptr is None:
-        return true
+      if ptr1 is None:
+        return True
       ptr1 = first
-      second = second.next
-    return false
+      second = second.nextval
+    return False
     
 a = Node(1)
 a.nextval = Node(2)
@@ -41,7 +41,7 @@ b.nextval.nextval.nextval.nextval.nextval = Node(4)
 
 c = SublistSearch()
 d = c.findList(a,b)
-if d == true:
+if d == True:
   print("Found")
 else:
   print("Not Found")
