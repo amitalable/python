8969d7264b298affbb013ff66a295908077309bc
Amit Ranjan <40341658+amitalable@users.noreply.github.com>
2019-08-01 17:13:21 +0530

    Update SublistSearch.py

--- SublistSearch.py
+++ SublistSearch.py
@@ -1,43 +1,33 @@
 class Node:
-  def __init__(self,dataval = None):
-    self.dataval = dataval
-    self.nextval = None
+    def __init__(self,dataval = None):
+        self.dataval = dataval
+        self.nextval = None
 class SublistSearch:
-  def findList(self,first,second):
-    ptr1 = first
-    ptr2 = second
-    #Printing first list
-    while ptr1 is not None:
-        print(ptr1.dataval,end=" ")
-        ptr1 = ptr1.nextval
-    print()
-    #Printing second list
-    while ptr2 is not None:
-        print(ptr2.dataval,end =" ")
-        ptr2 = ptr2.nextval
-    print()
-    
-    if first is None and second is None:
-        return True
-    if first is None or second is None:
-        return False
-      
-    while second is not None:
-        ptr2 = second
-        while ptr1 is not None:
-            if ptr2 is None:
-                return False
-            elif ptr1.dataval == ptr2.dataval:
-                ptr1 = ptr1.nextval
-                ptr2 = ptr2.nextval
-            else:
-                break;
-        if ptr1 is None:
-            return True
+    def findList(self,first,second):
         ptr1 = first
-        second = second.nextval
-    return False
+        ptr2 = second
+        if first is None and second is None:
+            return True
+        if first is None or (first is not None and second is None):
+            return False
+        while second is not None:
+            ptr2 = second
+            while ptr1 is not None:
+                if ptr2 is None:
+                    return False
+                elif (ptr1.dataval == ptr2.dataval):
+                    ptr1 = ptr1.nextval
+                    ptr2 = ptr2.nextval
+                else:
+                    break
+            if ptr1 is None:
+                return True
+            ptr1 = first
+            second = second.nextval
+        return False
+        
     
+        
 a = Node(1)
 a.nextval = Node(2)
 a.nextval.nextval = Node(3)
